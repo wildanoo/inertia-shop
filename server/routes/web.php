07cb29561a3d9cbde19f6c599b830d9655a73e3a
Al Biruni <birunidev@gmail.com>
2024-06-22 19:18:38 +0700

    modify route

--- server/routes/web.php
+++ server/routes/web.php
@@ -6,14 +6,11 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::redirect('/', '/dashboard');
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+Route::prefix('dashboard')->as('dashboard.')->middleware('auth')->group(function () {
+    Route::get('/', function () {
+        return Inertia::render('Dashboard');
+    })->middleware(['auth', 'verified'])->name('index');
 });
 
 require __DIR__ . '/auth.php';
