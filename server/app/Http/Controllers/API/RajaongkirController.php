b76b9dcbd5182f4cb94d35a08a485fe11e158acd
wildan <wabimantoro@gmail.com>
2025-08-29 18:22:46 +0700

    feat: rajaongkir api

---
+++ server/app/Http/Controllers/API/RajaongkirController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\City;
+use App\Models\Province;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+
+class RajaongkirController extends Controller
+{
+    public function migration()
+    {
+        $responseProvinces = Http::withHeader('key', env('RAJAONGKIR_API_KEY'))
+            ->get('https://rajaongkir.komerce.id/api/v1/destination/province');
+
+        foreach ($responseProvinces->json()['data'] as $province) {
+            $provinceData = Province::find($province["id"]);
+            if (!$provinceData) {
+                Province::create([
+                    'id' => $province['id'],
+                    'province' => $province['name'],
+                ]);
+            }
+        }
+
+        return response()->json([
+            'message' => 'Migration success',
+        ], 200);
+    }
+
+    public function provinces()
+    {
+        try {
+            $provinces = Province::all();
+            return $this->sendResponse($provinces, 'Provinces retrieved successfully');
+        } catch (\Throwable $th) {
+            return $this->sendError('Internal server error', [], 500);
+        }
+    }
+
+    public function city($provinceId)
+    {
+        try {
+            $path = 'https://rajaongkir.komerce.id/api/v1/destination/city/' . $provinceId;
+            $responseCity = Http::withHeader('key', env('RAJAONGKIR_API_KEY'))
+                ->get($path);
+            return $this->sendResponse($responseCity->json()['data'], 'Cities retrieved successfully');
+        } catch (\Throwable $th) {
+            return $this->sendError('Internal server error', [], 500);
+        }
+    }
+}
--- server/routes/api.php
+++ server/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\API\MediaController;
+use App\Http\Controllers\API\RajaongkirController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -11,3 +12,7 @@ Route::get('/user', function (Request $request) {
 Route::get('/media', [MediaController::class, 'index']);
 Route::post('/media', [MediaController::class, 'store']);
 Route::delete('/media/{id}', [MediaController::class, 'destroy']);
+
+Route::post('/rajaongkir/migration', [RajaongkirController::class, 'migration']);
+Route::get('/rajaongkir/provinces', [RajaongkirController::class, 'provinces']);
+Route::get('/rajaongkir/city/{id}', [RajaongkirController::class, 'city']);
